995766e436282579fbb446717192d2b68da3c0ac
Marco Spiess <marco.spiess@hotmail.de>
2017-01-21 19:18:00 +0100

    added some sprites

---
+++ Assets/Scripts/Firefly/FireflySizzle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflySizzle : MonoBehaviour {
+    public GameObject firefly;
+    public readonly int fireflyCount;
+    public float radius;
+    //private fireflyCountActual
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < fireflyCount; ++i)
+        {
+            Instantiate(firefly, new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius)), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
