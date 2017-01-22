10f3e0fbca89d72e8df924b65bc53e0bb9255259
Marco Spiess <marco.spiess@hotmail.de>
2017-01-22 03:15:55 +0100

    Added tobii EyeX support

--- Assets/Scripts/Firefly/FirefliesSwarm.cs
+++ Assets/Scripts/Firefly/FirefliesSwarm.cs
@@ -7,25 +7,52 @@ public class FirefliesSwarm : MonoBehaviour {
     public int fireflyCount;
     public float radius;
     public float mvSpeed;
+    private float useMvSpeed;
     //private fireflyCountActual
 
 	// Use this for initialization
 	void Start () {
+        // Check for eyetracker
+        Tobii.EyeTracking.DeviceStatus deviceStatus = Tobii.EyeTracking.EyeTrackingHost.GetInstance().EyeTrackingDeviceStatus;
+        if (deviceStatus == Tobii.EyeTracking.DeviceStatus.Pending)
+        {
+            Tobii.EyeTracking.EyeTracking.Initialize();
+        }
+        else if (deviceStatus == Tobii.EyeTracking.DeviceStatus.Tracking)
+        {
+
+        }
 		for(int i = 0; i < fireflyCount-1; ++i)
         {
             GameObject go = Instantiate(firefly, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
             go.transform.SetParent(this.gameObject.transform);
         }
+        useMvSpeed = mvSpeed;
 	}
 
     // Update is called once per frame
     void Update() {
-        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePoint;
+        //Tobii.EyeTracking.DeviceStatus deviceStatus = Tobii.EyeTracking.EyeTrackingHost.GetInstance().EyeTrackingDeviceStatus;
+        //Debug.Log(deviceStatus);
+        Tobii.EyeTracking.GazePoint gp = Tobii.EyeTracking.EyeTracking.GetGazePoint();
+
+        mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (gp.IsValid && gp.IsWithinScreenBounds)
+        {
+            mousePoint = Camera.main.ScreenToWorldPoint(new Vector3(gp.Screen.x, gp.Screen.y, 0.0f));
+            // Move faster when eyetracker is used
+            useMvSpeed = mvSpeed + 10.0f;
+        }
+        else
+        {
+            useMvSpeed = mvSpeed;
+        }
         Vector2 mvTarget = new Vector2(mousePoint.x, mousePoint.y);
         Vector2 currentPos = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
         Vector2 dist = mvTarget - currentPos;
-        if (dist.magnitude > 1.0f * mvSpeed * Time.deltaTime) {
-            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + dist.normalized.x * mvSpeed * Time.deltaTime, this.gameObject.transform.position.y + dist.normalized.y * mvSpeed * Time.deltaTime, this.gameObject.transform.position.z);
+        if (dist.magnitude > 1.0f * useMvSpeed * Time.deltaTime) {
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + dist.normalized.x * useMvSpeed * Time.deltaTime, this.gameObject.transform.position.y + dist.normalized.y * mvSpeed * Time.deltaTime, this.gameObject.transform.position.z);
         }
         else{
             this.gameObject.transform.position = new Vector3(mvTarget.x, mvTarget.y, this.gameObject.transform.position.z);
