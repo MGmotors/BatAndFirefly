0b2358784ddb7fea955f91de85622e265805d5c6
aminschubi <amin.schubhan@gmail.com>
2017-01-22 06:54:46 +0100

    proto

---
+++ Assets/Scripts/powerups/HealthPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(GameObject.FindGameObjectWithTag("Player").GetComponent<HealthScript>().currentHeards < 3)
+            GameObject.FindGameObjectWithTag("Player").GetComponent<HealthScript>().addHeard();
+
+        Destroy(gameObject);
+    }
+}
